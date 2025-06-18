5d48d575f52478569d8b568d61c7accfd67fc5c5
Henry <hlou@pedago.com>
2025-06-18 13:23:44 +0200

    Implemented useEffect for getting comapny profile and started on comapny dashboard

--- frontend/src/api.tsx
+++ frontend/src/api.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { CompanySearch } from "./company";
+import { CompanyProfile, CompanySearch } from "./company";
 
 interface SearchResponse {
     companies: CompanySearch[];
@@ -17,5 +17,14 @@ export const searchCompanies = async (query: string) => {
             console.error("Unexpected error:", error);
             return "An unexpected error occurred.";
         }
+    }
+}
+
+export const getCompanyProfile = async (query: string) => {
+    try {
+        const data = await axios.get<CompanyProfile[]>(`https://financialmodelingprep.com/api/v3/profile/${query}?apikey=${process.env.REACT_APP_API_KEY}`);
+        return data;
+    } catch (error: any) {
+        console.log("Error fetching company profile:", error.message);
     }
 }
